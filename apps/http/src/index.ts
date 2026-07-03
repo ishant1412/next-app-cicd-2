4c680cc7e4301ec8d3fbf57e712cb44bca173c9d
ishant1412 <ishant89572@gmail.com>
2026-07-03 18:35:55 +0530

    create yaml files and add info endpoint

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
   res.send("Hi there! HTTP server is running on port 3002");
 });
 
+app.get("/info",(req,res)=>{
+  res.send("hello my name is ishant khatri living in jhalawar peacefully hope the peace never dgoes aways i dont like bathing too much")
+})
 app.listen(PORT, () => {
   console.log(`HTTP Server is running on http://localhost:${PORT}`);
 });
