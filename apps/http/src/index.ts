2221eb5090ec409de38a15cd5cd8e27f7d90d842
ishant1412 <ishant89572@gmail.com>
2026-07-03 19:17:18 +0530

    edit info endpoint

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/info",(req,res)=>{
-  res.send("hello my name is ishant khatri living in jhalawar peacefully hope the peace never dgoes aways i dont like bathing too much")
+  res.send("hello my name is ishant khatri living in jhalawar peacefully hope the peace never dgoes aways ")
 })
 app.listen(PORT, () => {
   console.log(`HTTP Server is running on http://localhost:${PORT}`);
